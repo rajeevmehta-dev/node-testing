c88adc808d5f315997b4b1a856f4bf9cbb9908ba
RAJEEV-HP\Rajeev <rajeev.mehta@outlook.in>
2021-05-16 00:06:36 +0530

    Initial Project Setup

---
+++ test/start.js
@@ -0,0 +1,9 @@
+// const expect = require('chai').expect;
+// describe('Basic Testing Running', function() {
+
+//     it('Checking sum of two numbers', function() {
+//         const num1 = 1;
+//         const num2 =2;
+//         expect(num1+ num2).to.equal(3);
+//     })
+// });
